803834af0bd4eb76cd7d513e4da6e97a36eb69bf
kabir <rajnihappy25@gmail.com>
2025-06-26 19:05:51 +0530

    navbar hero upto about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import gsap from 'gsap'
 import {ScrollTrigger,SplitText} from "gsap/all";
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <div className=" flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300">
-            hello, GSAP!;
-        </h1>
-        </div>
+        <main>
+            <NavBar/>
+            <Hero/>
+           <Cocktails/>
+            <About/>
+            <Art/>
+        </main>
+
     )
 }
 export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,119 @@
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import {SplitText} from "gsap/all";
+import {ScrollTrigger} from "gsap/all";
+import {useRef} from "react";
+import {useMediaQuery} from "react-responsive";
+
+const Hero = () => {
+    const videoRef = useRef(null);
+    const isMobile = useMediaQuery({ maxWidth: 777 });
+    useGSAP(()=>{
+        gsap.registerPlugin(ScrollTrigger);
+        const heroSplit = new SplitText(".title", {type:'chars,words'});
+        const paraSplit = new SplitText(".subtitle", {type:'lines'});
+        heroSplit.chars.forEach((chars)=>{
+        chars.classList.add("text-gradient");});
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            ease:"expo.out",
+            duration: 1.8,
+            stagger: 0.06,
+
+        });
+        gsap.from(paraSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            ease:"expo.out",
+            duration: 1.8,
+            stagger: 0.06,
+            delay:1
+            }
+        );
+
+            gsap
+                .timeline({
+                    scrollTrigger: {
+                        trigger: "#hero",
+                        start: "top top",
+                        end: "bottom top",
+                        scrub: true,
+                    },
+                })
+                .to(".right-leaf", { y: 200 }, 0)
+                .to(".left-leaf", { y: -200 }, 0)
+
+        const videoStart = isMobile? "top 50%" : "center 60%";
+        const videoEnd = isMobile? "120% top" : "bottom top";
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: videoStart,
+                end: videoEnd,
+                scrub: true,
+                pin: true,
+            },
+        });
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
+
+
+
+        }
+
+            ,[])
+    return (
+      <>
+      <section id="hero" className="noisy">
+    <h1 className="title">MOJITO</h1>
+          <img
+           src="/images/hero-left-leaf.png"
+          className="left-leaf"
+          />
+          <img
+              src="/images/hero-right-leaf.png"
+              className="right-leaf"
+          />
+          <div className="body">
+              {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+
+              <div className="content">
+                  <div className="space-y-5 hidden md:block">
+                      <p>Cool. Crisp. Classic.</p>
+                      <p className="subtitle">
+                          Sip the Spirit <br /> of Summer
+                      </p>
+                  </div>
+
+                  <div className="view-cocktails">
+                      <p className="subtitle">
+                          Every cocktail on our menu is a blend of premium ingredients,
+                          creative flair, and timeless recipes — designed to delight your
+                          senses.
+                      </p>
+                      <a href="#cocktails">View cocktails</a>
+                  </div>
+              </div>
+          </div>
+
+      </section>
+          <div className="video absolute inset-0">
+              <video
+              src="/videos/input.mp4"
+              ref={videoRef}
+                muted
+              playsInline
+              preload="auto"
+
+
+              />
+          </div>
+      </>
+    )
+}
+export default Hero
